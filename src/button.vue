04f7139c64b7dfbc32f7100753c0730061b96df0
月轩 <394769450@qq.com>
2020-06-04 22:42:49 +0800

    添加icon iconDirection属性~

--- src/button.vue
+++ src/button.vue
@@ -1,21 +1,33 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button :class="{[`icon-${iconDirection}`]: true}" class="g-button">
+    <svg aria-hidden="true" class="icon" v-if="icon">
+      <use :xlink:href=`#s-${icon}`></use>
+    </svg>
+    <div class="message">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-    name: 'Button'
+    props: ['icon', 'iconDirection']
   }
 </script>
 
 <style lang="scss">
   .g-button {
     height: var(--button-height);
+    line-height: var(--button-height);
     font-size: var(--font-size);
-    padding: 0 1em;
+    padding: 0 .9em;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    vertical-align: middle;
+    align-items: center;
 
     &:hover {
       border-color: var(--border-color-hover);
@@ -28,5 +40,35 @@
     &:focus {
       outline: none;
     }
+
+    > .message {
+      order: 2;
+      margin: 0 0 0 .2em;
+    }
+
+    > .icon {
+      order: 1;
+    }
+
+    &.icon-right {
+      > .message {
+        order: 1;
+        margin: 0 .2em 0 0;
+      }
+
+      > .icon {
+        order: 2;
+      }
+    }
+
+    .icon {
+      width: 1em;
+      height: 1em;
+      vertical-align: -0.15em;
+      fill: currentColor;
+      overflow: hidden;
+    }
   }
+
+
 </style>

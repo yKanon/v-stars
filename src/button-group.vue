8ed57df51debe7fd6d455e10b7ef4662dadec6d8
月轩 <394769450@qq.com>
2020-06-06 17:31:05 +0800

    第一个子节点不需要左移 1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -31,7 +31,10 @@
 
     .s-button {
       border-radius: 0;
-      margin-left: -1px;
+
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
 
       &:first-child {
         border-top-left-radius: var(--border-radius);

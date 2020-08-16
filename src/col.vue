0eccb6289e427df15c3b25979bfcc360ecaa35b6
月轩 <394769450@qq.com>
2020-08-16 20:04:41 +0800

    row & col base

---
+++ src/col.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col" :class="[`col-${span}`]"></div>
+</template>
+
+<script>
+  export default {
+    name: 'StarsCol',
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $class-prefix: col-;
+
+  .col {
+    height: 100px;
+    width: 50%;
+    background-color: grey;
+    border: 1px solid green;
+
+    @for $n from 1 through 24
+    {
+      &.#{$class-prefix}#{$n}
+      {
+        width:($n / 24) * 100%;
+      }
+    }
+  }
+
+</style>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('s-button', Button)
 Vue.component('s-button-group', ButtonGroup)
 Vue.component('s-icon', Icon)
 Vue.component('s-input', Input)
+Vue.component('s-row', Row)
+Vue.component('s-col', Col)
 
 new Vue({
   el: '#app',

e5cc75e9c9249b7fe30ff7d0136645e44da41eed
月轩 <394769450@qq.com>
2020-08-17 10:33:06 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="col" :class="[`col-${span}`]"></div>
+  <div :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}"
+       class="col"
+  >
+    <div style="border: 1px solid green; height: 100%">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,26 +14,43 @@
     name: 'StarsCol',
     props: {
       span: {
+        type: [Number, String],
+        validator (value) {
+          return parseInt(value) <= 24
+        }
+      },
+      offset: {
         type: [Number, String]
       }
-    }
+    },
+    // mounted () {
+    //   console.log(this.gutter)
+    // },
+    data() {
+      return {
+        gutter: 0
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   $class-prefix: col-;
+  $offset-prefix: offset-;
 
   .col {
     height: 100px;
     width: 50%;
-    background-color: grey;
-    border: 1px solid green;
+    /*padding: 0 10px;*/
+    /*margin: 0 10px;*/
 
-    @for $n from 1 through 24
-    {
-      &.#{$class-prefix}#{$n}
-      {
-        width:($n / 24) * 100%;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+
+      &.#{$offset-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'StarsRow'
+    name: 'StarsRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted () {
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+        console.log(vm)
+      })
+    },
+    methods: {
+      // getColValue () {
+      //   const span = this.$refs.col.span
+      //   console.log(span)
+      // }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   .row {
     display: flex;
+    box-sizing: border-box;
+    margin: 0 -10px;
   }
 </style>

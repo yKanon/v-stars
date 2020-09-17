570691d034e979dc4598616174d4e623fca58e25
月轩 <394769450@qq.com>
2020-09-17 18:56:32 +0800

    tab 切换动画完成

--- src/tabs-nav.vue
+++ src/tabs-nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tabs-nav">
     <slot></slot>
-    <div class="line" ref="line"></div>
+    <div class="line" ref="line" v-show="visible"></div>
     <div class="action-wrapper">
       <slot name="action"></slot>
     </div>
@@ -9,36 +9,44 @@
 </template>
 
 <script>
-export default {
-  name: "StarsTabsNav",
-  inject: ["eventBus"],
-  mounted() {
-    this.eventBus.$on("update:selected", (name, itemVm) => {
-      console.log(name);
-      console.log(itemVm);
-    });
-  },
-};
+  export default {
+    name: 'StarsTabsNav',
+    inject: ['eventBus'],
+    data () {
+      return {
+        visible: false
+      }
+    },
+    mounted () {
+      this.eventBus.$on('update:selected', (name, itemVm) => {
+        let { width, left } = itemVm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.transform = `translateX(${left}px)`
+        this.visible = true
+      })
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
-$tab-height: 40px;
-$border-bottom-color: blue;
-.tabs-nav {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  border: 1px solid red;
-  height: $tab-height;
-  position: relative;
-  > .line {
-    position: absolute;
-    bottom: 0;
-    width: 100px;
-    border-bottom: 2px solid $border-bottom-color;
+  $tab-height: 40px;
+  $border-bottom-color: blue;
+  .tabs-nav {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: $tab-height;
+    position: relative;
+
+    > .line {
+      position: absolute;
+      bottom: 0;
+      border-bottom: 1px solid $border-bottom-color;
+      transition: all .3s;
+    }
+
+    > .action-wrapper {
+      margin-left: auto;
+    }
   }
-  > .action-wrapper {
-    margin-left: auto;
-  }
-}
 </style>
